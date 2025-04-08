01b0ea3436c603977d425a83c41e8e69f5d945bf
technickle <github@nicklin.info>
2025-04-08 17:03:48 -0400

    switch images to unoptimized

--- web/next.config.js
+++ web/next.config.js
@@ -11,6 +11,7 @@ const nextConfig = {
   },
   images: {
     formats: ["image/avif", "image/webp"],
+    unoptimized: true,
   },
   basePath: isProd ? '/jhu-measles-tracker' : '',
   assetPrefix: isProd ? '/jhu-measles-tracker/' : '',
