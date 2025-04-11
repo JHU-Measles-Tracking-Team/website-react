961bfbf0094e5538ef13781d4ae019a2468ceed9
technickle <github@nicklin.info>
2025-04-11 14:08:47 -0400

    fix google tag manager for real?

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -8,13 +8,17 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GTMID}`}
+          />
+          <script
             dangerouslySetInnerHTML={{
-              __html:
-                `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':` +
-                `new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],` +
-                `j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=` +
-                `'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);` +
-                `})(window,document,'script','dataLayer','${GTMID}');`,
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GTMID}');
+          `,
             }}
           />
           {/* Pulling fonts into the project has been moved to App using next/fonts, but we can revert to this method if necessary */}
